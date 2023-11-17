e1ca1b4c5cc72ff331e999b90ca799bf9281d32d
Robert Koval <kovalrobert@me.com>
2023-11-17 21:08:36 +0200

    Initial Commit

---
+++ Vapor Leaf Syntax Highlight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vapor Leaf Syntax Highlight
+//
+//  Created by Robert Koval on 17.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vapor Leaf Syntax Highlight/Vapor_Leaf_Syntax_HighlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_Leaf_Syntax_HighlightApp.swift
+//  Vapor Leaf Syntax Highlight
+//
+//  Created by Robert Koval on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_Leaf_Syntax_HighlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
